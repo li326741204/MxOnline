9f4d015eddcd2843802c11a00723642133f76971
874274146 <874274146@qq.com>
2019-12-10 23:27:40 +0800

    第一个版本

---
+++ apps/courses/adminx.py
@@ -0,0 +1,36 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'liliang'
+__date__ = '2019-12-06 22:36'
+
+import xadmin
+from .models import Course, Lesson, Vedio, CourseResource
+
+
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class VedioAdmin(object):
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson__name', 'name', 'add_time']  # 双下划线表示在过滤选项中显示该外键字段
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course', 'name', 'download', 'add_time']
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Vedio, VedioAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
